d2459806d93ac9f39cacc68e363bc84d91540ddf
yoshihara <h.yoshihara@everyleaf.com>
2015-10-06 17:39:10 +0900

    Fix prepare_embulk for Embulk 0.7

--- test/prepare_embulk.rb
+++ test/prepare_embulk.rb
@@ -1,10 +1,17 @@
-require "embulk/command/embulk_run"
+module PrepareEmbulk
+  require "embulk/command/embulk_run"
 
-classpath_dir = Embulk.home("classpath")
-jars = Dir.entries(classpath_dir).select{|f| f =~ /\.jar$/ }.sort
-jars.each do |jar|
-  require File.join(classpath_dir, jar)
+  if Embulk.respond_to?(:home)
+    # keep compatibility for Embulk 0.6.x
+    classpath_dir = Embulk.home("classpath")
+    jars = Dir.entries(classpath_dir).select{|f| f =~ /\.jar$/ }.sort
+    jars.each do |jar|
+      require File.join(classpath_dir, jar)
+    end
+    require "embulk/java/bootstrap"
+    require "embulk"
+  else
+    require "embulk"
+    Embulk.setup
+  end
 end
-require "embulk/java/bootstrap"
-
-require "embulk"
